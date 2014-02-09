fbc77f220b1ec61e3fc527a23fdde3391b9992b6
Daniel Jędrusik <danjed@student.agh.edu.pl>
2014-02-09 19:19:15 +0100

    dodany model comment, zmieniony widok daty

---
+++ app/models/post.rb
@@ -0,0 +1,22 @@
+class Post
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Taggable
+
+  field :body, type: String
+  field :title, type: String
+  field :archived, type: Boolean, default: false
+
+  has_many :comments
+
+  validates_presence_of :body, :title
+
+  belongs_to :user
+
+  default_scope ->{ ne(archived: true) }
+
+  def archive!
+    update_attribute :archived, true
+  end
+
+end
